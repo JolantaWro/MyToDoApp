53e26ab505bbc3a4943ce0b295949dfbf7a268c7
JolantaWro <jolanta.woronowska@gmail.com>
2023-05-01 18:47:50 +0200

    update reducer

--- my-app/src/redux/reducer.js
+++ my-app/src/redux/reducer.js
@@ -4,6 +4,21 @@ const initialStation = [];
 
 function reducerTasks (state=initialStation, action) {
     switch (action.type){
+        case ADD_TASK:
+            const taskId = maxIdTasks(state)
+            return [...state, {id: taskId, ...action.payload}]
+        case REMOVE_TASK:
+            return state.filter(element => element.id !== action.payload.id);
+        case CHANGE_TASK:
+            return state.map(todo => {
+                if(todo.id !== action.payload.id) {
+                    return todo;
+                }
+                return {
+                    ...todo,
+                    open: !todo.open
+                }
+            })
         default:
             return state
     }
