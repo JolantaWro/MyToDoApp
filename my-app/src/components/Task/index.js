c79709f0d230ad6d5dbbbdb1fa892de3a4a62c57
JolantaWro <jolanta.woronowska@gmail.com>
2023-05-08 20:55:35 +0200

    up css

--- my-app/src/components/Task/index.js
+++ my-app/src/components/Task/index.js
@@ -6,11 +6,11 @@ const AllTasks = ({tasks, removeTask, changeTask}) => {
 
     return (
         <>
-            {tasks.length === 0 && <div className="panel-block">Brak wpisów!</div>}
+            {tasks.length === 0 && <div className="note">Brak wpisów!</div>}
 
             {tasks.length > 0 && tasks.map(el => (<>
                     <li>
-                        <Link to={`/${el.id}`} key={el.id}>{el.title}<br/>{el.description}</Link>
+                        <Link to={`/${el.id}`} key={el.id} className="note">{el.title}<br/>{el.description}</Link>
                         <button onClick={() => removeTask(el)}>Usuń</button>
                         <button onClick={() => changeTask(el)}>Zrobione</button>
                     </li>
