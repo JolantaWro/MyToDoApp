7d3809444f59f0422f1bcbc185839bed8e336b01
JolantaWro <jolanta.woronowska@gmail.com>
2023-05-11 20:10:45 +0200

    css

--- my-app/src/components/Task/AddTask.js
+++ my-app/src/components/Task/AddTask.js
@@ -19,23 +19,25 @@ const AddTask = ({onSubmitTask}) => {
 
 
     return (
-        <div className="card shadow">
-            <div className="card-body">
-                <h1 className="card-title">Nowe zadanie</h1>
-                <form onSubmit={handleSubmit}>
-                    <div className="form-group">
-                        <input
-                            type="text" className="form-control" name="title" placeholder="Title" value={form.title}
-                            onChange={handleChangeInput}/>
-                    </div>
-                    <div className="form-group">
-                        <input type="text" className="form-control" name="description" placeholder="Description" value={form.description}
-                               onChange={handleChangeInput}/>
-                    </div>
-                    <button className="btn btn-info">Dodaj zadanie do listy
-                        <i className="fas fa-plus-circle ml-1"></i>
-                    </button>
-                </form>
+        <div className="note">
+            <div className="card shadow">
+                <div className="card-body">
+                    <h1 className="card-title">Nowe zadanie</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <input
+                                type="text" className="form-control" name="title" placeholder="Title" value={form.title}
+                                onChange={handleChangeInput}/>
+                        </div>
+                        <div className="form-group">
+                            <input type="text" className="form-control" name="description" placeholder="Description" value={form.description}
+                                   onChange={handleChangeInput}/>
+                        </div>
+                        <button className="btn btn-info">Dodaj zadanie do listy
+                            <i className="fas fa-plus-circle ml-1"></i>
+                        </button>
+                    </form>
+                </div>
             </div>
         </div>
     );
